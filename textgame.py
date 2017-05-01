177d97842d31a4301c4f65a8aa34ed09ac29e0c3
Josh Jessop-smith <joshjessop-smith@Joshs-MBP.lan>
2017-05-01 16:31:22 +1000

    Added another game state, game now finishes... Though at this stage calling it a game is a bit of a stretch

--- textgame.py
+++ textgame.py
@@ -1,6 +1,6 @@
 import time
 
-def game():
+def intro():
 	state = False
 
 	print("\n"*5)
@@ -31,9 +31,9 @@ def game():
 		elif inp in greetings:
 			print("{} back at you!".format(inp.capitalize()))
 		elif inp in rude:
-			time.sleep(1)
+			#time.sleep(1)
 			print("\n...")
-			time.sleep(1)
+			#time.sleep(1)
 			print("\n...That wasn't a very nice thing to say")
 		elif inp[0] in number:
 			print("So you're a maths nerd huh?")
@@ -43,4 +43,31 @@ def game():
 			print("Well that isn't even a word")
 
 
+def game():
+	time.sleep(1)
+	print("\n...")
+	time.sleep(1)
+	print("\n...")
+	time.sleep(1)
+	print("\n...")
+	time.sleep(2)
+	print("\nThe computer seems to be initialising something")
+	time.sleep(1)
+	print("\nQuick say the first thing that pops into your head!")
+	time.sleep(5)
+	print("\nYou have to physically say it into the laptop microphone")
+	time.sleep(8)
+	print("\nWow that's a really weird thing to be thinking")
+	time.sleep(2)
+	print("\nAlso this program can't actually register your voice or anything")
+	time.sleep(2)
+	print("\nI just made you say something random and look like a weirdo")
+	time.sleep(2)
+	print("\nin fact that was really what all this was about")
+	time.sleep(2)
+	print("\nCongratulations! You win the game")
+	time.sleep(2)
+	print("\nBut at what cost?\n")
+
+intro()
 game()
